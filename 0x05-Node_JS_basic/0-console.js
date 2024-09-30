73cdd34a90a15f493cfc2aa21d11cb14fc11f8b7
hidaya <hidaya.regragui@gmail.com>
2024-09-30 16:49:22 +0100

    doc

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,8 +3,13 @@
  * @param {string} message - The message to log.
  * @returns {void}
  */
-function displayMessage(message) {
-    console.log(message);
-}
+// Arrow function
+const displayMessage = (message) => console.log(message);
 
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
 module.exports = displayMessage;
