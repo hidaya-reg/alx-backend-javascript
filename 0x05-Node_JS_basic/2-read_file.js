ac237596677abdf55ebe7016fd01e1e10ebccb3f
hidaya <hidaya.regragui@gmail.com>
2024-10-01 09:14:03 +0100

    task2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+
+    if (lines.length <= 1){
+      throw new Error('Cannot load the database');
+    }
+
+    const headers = lines.shift();
+
+    const studentsByField = {};
+    let totalStudents = 0;
+
+    lines.forEach(line => {
+      const [firstname, lastname, age, field] = line.split(',');
+      if (firstname && lastname && age && field) {
+        totalStudents++;
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
+        }
+        studentsByField[field].push(firstname);
+      }
+    });
+    console.log(`Number of students: ${totalStudents}`);
+    for (const [field, students] of Object.entries(studentsByField)) {
+        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (error) {
+      throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
