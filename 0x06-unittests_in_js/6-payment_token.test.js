a7a64a365e4d9b41d0552c6c04acdf7777cf407f
hidaya <hidaya.regragui@gmail.com>
2024-10-04 08:34:02 +0100

    task6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,24 +1,14 @@
+// 6-payment_token.test.js
 const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
+  it('should return the correct object when success is true', (done) => {
     getPaymentTokenFromAPI(true)
       .then((res) => {
         expect(res).to.deep.equal({ data: 'Successful response from the API' });
         done();
       })
-      .catch((err) => done(err)); // Handle any potential errors
-  });
-
-  it('getPaymentTokenFromAPI(success), where success == false', (done) => {
-    getPaymentTokenFromAPI(false)
-      .then(() => {
-        done(new Error('Expected method to reject.')); // Fail if it resolves
-      })
-      .catch((err) => {
-        expect(err).to.be.an('error'); // Ensure an error is thrown
-        done();
-      });
+      .catch((error) => done(error));  // Handle potential errors
   });
 });
